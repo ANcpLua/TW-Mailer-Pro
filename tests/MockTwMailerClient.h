526acd9e6631391f3b14cdbff6f57a14bbaa333e
Alexander Nachtmann <anfh22@outlook.com>
2024-07-09 05:41:58 +0200

    Update MockTwMailerClient.h

--- tests/MockTwMailerClient.h
+++ tests/MockTwMailerClient.h
@@ -9,35 +9,17 @@ class MockTwMailerClient : public TwMailerClient
 public:
     MockTwMailerClient (const std::string &ldap_uri, uint16_t port)
             : TwMailerClient (ldap_uri, port)
-    {
-    }
+    {}
 
-    MOCK_METHOD (void, connect, (), (override)
-
-    );
-
-    MOCK_METHOD (void, disconnect, (), (override)
-
-    );
-
-    MOCK_METHOD (void, sendCommand, (const std::string &command, bool debug),
-    (override)
-
-    );
+    MOCK_METHOD (void, connect, (), (override));
+    MOCK_METHOD (void, disconnect, (), (override));
+    MOCK_METHOD (void, sendCommand, (const std::string &command, bool debug), (override));
     MOCK_METHOD (std::string, receiveResponse, (bool debug), (override));
     MOCK_METHOD (std::string, getInput, (const std::string &prompt), (override));
-    MOCK_METHOD (std::string, getMaskedInput, (const std::string &prompt),
-    (override));
-    MOCK_METHOD (std::string, getMultiLineInput, (const std::string &prompt),
-    (override));
-
-    MOCK_METHOD (bool, validateEmail, (const std::string &email), (override)
-
-    );
-
-    MOCK_METHOD (bool, validateSubject, (const std::string &subject), (override)
-
-    );
+    MOCK_METHOD (std::string, getMaskedInput, (const std::string &prompt), (override));
+    MOCK_METHOD (std::string, getMultiLineInput, (const std::string &prompt), (override));
+    MOCK_METHOD (bool, validateEmail, (const std::string &email), (override));
+    MOCK_METHOD (bool, validateSubject, (const std::string &subject), (override));
 
     using TwMailerClient::handleDelete;
     using TwMailerClient::handleList;
